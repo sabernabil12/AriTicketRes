12024ed315e354e45e5d6b944d26661fc821760f
sabernabil12 <SABERNABIL12@GMAIL.COM>
2017-09-06 10:08:11 -0700

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include "Menu.h"
+#include "Plane.h"
+
+using namespace std;
+
+
+int main(int argc, char* argv[])
+{
+
+	char dummy;
+
+	CSC300::Menu m;
+
+	m.run();
+
+	cin >> dummy;
+
+	return 0;
+}
